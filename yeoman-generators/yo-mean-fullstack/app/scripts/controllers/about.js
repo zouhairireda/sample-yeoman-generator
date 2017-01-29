0a1ef1a2db59ab64d6dc879bee65039290560569
Reda ZOUHAIRI <zouhairireda@gmail.com>
2017-01-29 22:27:09 +0000

    commit first sample for generating angularjs project, fullstack js project, gulp project with YO generator

---
+++ yeoman-generators/yo-mean-fullstack/app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name firstyoangularApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the firstyoangularApp
+ */
+angular.module('firstyoangularApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ yeoman-generators/yo-mean-fullstack/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name firstyoangularApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the firstyoangularApp
+ */
+angular.module('firstyoangularApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
